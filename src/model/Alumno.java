6b321eb437d40afaa152801916aeb228d9a5b438
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-04 09:14:42 -0500

    commit-Alumno .

--- src/model/Alumno.java
+++ src/model/Alumno.java
@@ -2,5 +2,71 @@ package model;
 
 import javax.xml.bind.annotation.*;
 
+@XmlRootElement(name= "alumno")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Alumno {
+
+    private int id;
+    private int idAlumno;
+    private int idCurso;
+    private String nombre;
+    private String sexo;
+    private int edad;
+    private String direccion;
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
