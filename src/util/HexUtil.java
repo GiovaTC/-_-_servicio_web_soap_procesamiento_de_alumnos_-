ef059225678463e5a4cd58c37c38eb564b0c9629
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-04 09:19:21 -0500

    commit-HexUtil / .

--- src/util/HexUtil.java
+++ src/util/HexUtil.java
@@ -1,4 +1,13 @@
 package util;
 
 public class HexUtil {
-}
+
+    public static String toHex(String input) {
+        StringBuilder hex = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            hex.append(String.format("%02X", (int) c));
+        }
+        return hex.toString();
+    }
+}   
+
